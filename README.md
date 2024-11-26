f80118a11828d8b4545fd097a8cd987479f07445
James Kerr <kerr@hey.com>
2024-11-26 09:57:39 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,50 @@
+# SuperDB Wasm
+
+SuperDB Wasm is a JavaScript module that runs in a browser and exposes a JS version of the `super` command.
+
+## Installation
+
+This is an ESM module for the browser. To use it, import it from the local file system, a CDN, or an import map. Here’s an example of importing from jsdelivr.
+
+```js
+import { SuperDB } from "https://cdn.jsdelivr.net/npm/superdb-wasm/index.js";
+```
+
+## Usage Example
+
+Call `.instantiate(url)` on the SuperDB class and pass in the URL of of the wasm file. The wasm file is named "superdb.wasm" at the root of the package.
+
+```js
+const superdb = await SuperDB.instantiate(
+  "https://cdn.jsdelivr.net/npm/superdb-wasm/superdb.wasm"
+);
+```
+
+This will fetch, decompress, and run the wasm code returning an instance of the SuperDB class.
+
+Once you have an instance, call the `run({input, query})` method to start querying data.
+
+```js
+const result = await superdb.run({
+  query: "yield this + 100",
+  input: "1 2 3",
+});
+
+console.log(result);
+// Prints:
+// 100
+// 200
+// 300
+```
+
+## Contributing
+
+The only purpose of this library is to wrap the superdb.wasm file. To test the code, simply open the index.html file served from a static file server and ensure that the code runs with no errors in the console. There are no automated tests and the code should remain simple enough not to require them.
+
+```
+npx serve .
+```
+
+Then open http://localhost:3000
+
+Ensure the numbers "101 102 103" appear on the page. This means the wasm file was successfully used to add 100 to the numbers 1, 2, and 3.
---
+++ index.js
@@ -0,0 +1,75 @@
+import "../lib/wasm_exec.js";
+import "../lib/bridge.js";
+
+export class SuperDB {
+  /**
+   * Instantiates a new `SuperDB` instance from a given wasm file URL.
+   * @static
+   * @param {string} url - The URL of the wasm file to instantiate.
+   * @returns {Promise<SuperDB>} A promise that resolves to a new `SuperDB` instance.
+   */
+  static instantiate(url) {
+    return this.fetchCompressed(url)
+      .then((resp) => this.createInstance(resp))
+      .then((instance) => new SuperDB(instance));
+  }
+
+  /**
+   * @private
+   */
+  static fetchCompressed(url) {
+    const headers = { "Content-Type": "application/wasm" };
+    const gzip = new DecompressionStream("gzip");
+
+    return fetch(url)
+      .then((resp) => resp.blob())
+      .then((blob) => blob.stream().pipeThrough(gzip))
+      .then((stream) => new Response(stream, { headers }));
+  }
+
+  /**
+   * @private
+   */
+  static async createInstance(response) {
+    const go = new Go();
+    const env = go.importObject;
+    const wasm = await WebAssembly.instantiateStreaming(response, env);
+    go.run(wasm.instance);
+    return __go_wasm__;
+  }
+
+  /**
+   * @private
+   */
+  constructor(instance) {
+    this.instance = instance;
+  }
+
+  /**
+   * Executes a query using the provided options and returns the result.
+   * @async
+   * @param {Object} opts - The options for the query.
+   * @param {string} [opts.query] - The ZQ program to execute.
+   * @param {string | ReadableStream} [opts.input] - The input data for the query.
+   * @param {'auto' | 'arrows' | 'csv' | 'json' | 'line' | 'parquet' | 'tsv' | 'vng' | 'zeek' | 'zjson' | 'zng' | 'zson'} [opts.inputFormat] - The format of the input data.
+   * @param {'auto' | 'arrows' | 'csv' | 'json' | 'line' | 'parquet' | 'tsv' | 'vng' | 'zeek' | 'zjson' | 'zng' | 'zson'} [opts.outputFormat] - The desired format of the output data.
+   * @returns {Promise<any[]>} A promise that resolves to the processed query result.
+   */
+  run(args) {
+    return this.instance.zq({
+      input: args.input,
+      inputFormat: args.inputFormat,
+      program: args.query,
+      outputFormat: args.outputFormat,
+    });
+  }
+
+  /**
+   * Parses the given query string and returns the result.
+   * @param {string} query - The query string to parse.
+   * @returns {any} The parsed result.
+   */
+  parse(query) {
+    return this.instance.parse(query);
+  }
+}
